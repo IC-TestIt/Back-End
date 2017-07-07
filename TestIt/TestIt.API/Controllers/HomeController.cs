2eb378a05979cc4355bc0df57cbd6e90804fbb95
vitormdias <vitor.dias@ventron.com.br>
2017-07-07 10:20:27 -0300

    Add deploy to azure

---
+++ TestIt/TestIt.API/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestIt.API.Controllers
+{
+    [Route("api")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Welcome to Test It!";
+        }
+    }
+}
