d9c313411c1ebe1b92f4b306a7a708ee1641c41b
Vítor Manfré <vitormd97@gmail.com>
2017-07-11 14:24:43 -0300

    Feat/add auto mapper (#7)
    * create user view model added
    
    * create user viewmodel validator added
    
    * startup completed
    
    * references updated
    
    * automapper configuration added

---
+++ TestIt.API/ViewModels/Mappings/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using TestIt.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestIt.API.ViewModels.Mappings
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+            //Mapper.CreateMap<Schedule, ScheduleViewModel>()
+            //   .ForMember(vm => vm.Creator,
+            //        map => map.MapFrom(s => s.Creator.Name))
+            //   .ForMember(vm => vm.Attendees, map =>
+            //        map.MapFrom(s => s.Attendees.Select(a => a.UserId)));
+        }
+    }
+}
---
+++ TestIt.API/ViewModels/Validations/User/CreateUserViewModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using TestIt.API.ViewModels.User;
+
+namespace TestIt.API.ViewModels.Validations.User
+{
+    public class CreateUserViewModelValidator : AbstractValidator<CreateUserViewModel>
+    {
+        public CreateUserViewModelValidator()
+        {
+            RuleFor(user => user.Name).NotEmpty().WithMessage("Name cannot be empty");
+            RuleFor(user => user.Email).NotEmpty().WithMessage("Email cannot be empty");
+            RuleFor(user => user.Password).NotEmpty().WithMessage("Password cannot be empty");
+            RuleFor(user => user.IdentifyerType).NotEmpty().WithMessage("Identifyer Type cannot be empty");
+        }
+    }
+}
