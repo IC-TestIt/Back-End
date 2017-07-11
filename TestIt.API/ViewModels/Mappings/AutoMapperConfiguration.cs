878a998670dd02c8f46fe8a9a6bbcc4e0002e376
vitormdias <vitor.dias@ventron.com.br>
2017-07-11 14:17:39 -0300

    automapper configuration added

---
+++ TestIt.API/ViewModels/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace TestIt.API.ViewModels.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DomainToViewModelMappingProfile>();
+                x.AddProfile<ViewModelToDomainMappingProfile>();
+            });
+        }
+    }
+}
---
+++ TestIt.API/ViewModels/Mappings/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace TestIt.API.ViewModels.Mappings
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        protected override void Configure()
+        {
+
+            //Mapper.CreateMap<ScheduleViewModel, Schedule>()
+            //   .ForMember(s => s.Creator, map => map.UseValue(null))
+            //   .ForMember(s => s.Attendees, map => map.UseValue(new List<Attendee>()));
+
+            //Mapper.CreateMap<UserViewModel, User>();
+            Mapper.CreateMap<User.CreateUserViewModel, Model.Entities.User>();
+        }
+    }
+}
