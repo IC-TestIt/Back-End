fc1d24cc9e62c33f0a29421f39722a1d0272937f
vitormdias <vitor.dias@ventron.com.br>
2017-07-12 16:43:41 -0300

    class renamed

--- TestIt.Data/Abstract/IRepositories.cs
+++ TestIt.Data/Abstract/IRepositories.cs
@@ -5,7 +5,7 @@ namespace TestIt.Data.Abstract
     public interface IUserRepository : IEntityBaseRepository<User> {}
     public interface ITeacherRepository : IEntityBaseRepository<Teacher> {}
     public interface IStudentRepository : IEntityBaseRepository<Student> {}
-    public interface ISocialIdRepository : IEntityBaseRepository<SocialId> {}
+    public interface ISocialIdRepository : IEntityBaseRepository<SocialIdentifier> {}
     public interface IOrganizationRepository : IEntityBaseRepository<Organization> {}
     public interface IClassRepository : IEntityBaseRepository<Class> {}
     
--- TestIt.Data/Repositories/SocialIdRepository.cs
+++ TestIt.Data/Repositories/SocialIdRepository.cs
@@ -5,7 +5,7 @@ using TestIt.Model.Entities;
 
 namespace TestIt.Data.Repositories
 {
-    public class SocialIdRepository : EntityBaseRepository<SocialId>, ISocialIdRepository
+    public class SocialIdRepository : EntityBaseRepository<SocialIdentifier>, ISocialIdRepository
     {
         public SocialIdRepository(TestItContext context)
             : base(context)
