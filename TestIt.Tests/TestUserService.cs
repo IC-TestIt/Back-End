df19ea4b873063476d274c59efc5f5b0476b78de
vitormdias <vitor.dias@ventron.com.br>
2017-07-13 16:10:11 -0300

    Adiciona teste para a UserService

---
+++ TestIt.Tests/TestUserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestIt.Business;
+using TestIt.Model.Entities;
+using TestIt.Tests.MockRepositories;
+using Xunit;
+
+namespace TestIt.Tests
+{
+    public class TestUserService
+    {
+        public IUserService service;
+        public MockUserRepository repository;
+
+        public TestUserService()
+        {
+            repository = new MockUserRepository();
+            service = new Business.Services.UserService(repository);
+        }
+
+        [Fact]
+        public void TestValidLogin()
+        {
+            var result = service.ValidLogin("test@email.com", "test");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void TestInvalidLogin()
+        {
+            var result = service.ValidLogin("test@email", "pswd");
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void TestEmptyLogin()
+        {
+            var result = service.ValidLogin("", "");
+
+            Assert.False(result);
+        }
+    }
+}
