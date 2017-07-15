3b69c9bb93798df0e5610b81692ebece5f030063
medson10 <medson.oliveira@fatec.sp.gov.br>
2017-07-15 01:32:58 -0300

    Adiciona principais metodos

--- TestIt.Business/Services/UserService.cs
+++ TestIt.Business/Services/UserService.cs
@@ -1,4 +1,6 @@
-﻿using TestIt.Data.Abstract;
+﻿using System.Collections.Generic;
+using TestIt.Data.Abstract;
+using TestIt.Model.Entities;
 
 namespace TestIt.Business.Services
 {
@@ -13,6 +15,43 @@ namespace TestIt.Business.Services
         public bool ValidLogin(string email, string pswd)
         {
             return userRepository.Any(x => x.Email == email && x.Password == pswd);
+        }
+
+        public IEnumerable<User> Get() 
+        {
+            return userRepository.GetAll();
+        }
+
+        public User GetSingle(int id)
+        {
+            return userRepository.GetSingle(id);
+        }
+
+        public void Save(User t) 
+        {
+            userRepository.Add(t);
+            userRepository.Commit();
+        }
+
+        public void Delete(int id)
+        {
+            User t = userRepository.GetSingle(id);
+
+            if(t != null) 
+            {
+                userRepository.DeleteWhere(x => x.Id == t.Id);
+                userRepository.Commit();
+            }
+        }
+
+        public void Update(int id, User user)
+        {
+            User t = userRepository.GetSingle(id);
+            if (t != null && user.Id == t.Id)
+            {
+                userRepository.Update(user);
+                userRepository.Commit();
+            }
         }
     }
 }
