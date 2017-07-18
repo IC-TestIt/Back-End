53d5195a79f045fd419ed0e795b99b6b7d7ed37a
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-07-17 21:17:21 -0300

    Adiciona criar turma

---
+++ TestIt.API/Controllers/ClassController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TestIt.API.ViewModels.Mappings;
+using TestIt.API.ViewModels.Class;
+using TestIt.Business;
+using TestIt.Model.Entities;
+
+namespace TestIt.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ClassController : Controller
+    {
+        private Business.IClassService classService;
+
+        public ClassController(IClassService classService)
+        {
+            this.classService = classService;
+        }
+
+        [HttpPost]
+        public void Post([FromBody]CreateClassViewModel viewModel)
+        {
+            Class newClass = Mapper.Map<Class>(viewModel);
+
+            classService.Save(newClass);
+        }
+
+
+    }
+}
