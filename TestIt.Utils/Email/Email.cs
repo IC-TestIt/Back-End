96008e2c933d4bb78c1ce43dcb9a8e8947a0392e
Medson de Oliveira Junior <medson.oliveira@fatec.sp.gov.br>
2017-07-19 14:28:03 -0300

    Adiciona camada de email

---
+++ TestIt.Utils/Email/Email.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestIt.Utils.Email
+{
+    public class Email
+    {
+        public string ToAdress { get; set; }
+        public string ToAdressTitle { get; set; }
+        public string Subject { get; set; }
+        public string BodyContent { get; set; }
+    }
+}
