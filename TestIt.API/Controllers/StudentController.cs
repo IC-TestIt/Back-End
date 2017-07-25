7b4c2903fc9c3328618558630a12ae673542cb13
Medson de Oliveira Junior <medson.oliveira@fatec.sp.gov.br>
2017-07-25 00:23:39 -0300

    Adiciona Student Controller

---
+++ TestIt.API/Controllers/StudentController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestIt.Business;
+
+namespace TestIt.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class StudentController : Controller 
+    {
+        private IStudentService studentService;
+        private IUserService userService;
+
+        public StudentController(IStudentService studentService, IUserService userService)
+        {
+            this.studentService = studentService;
+            this.userService = userService;
+        }
+
+        [HttpGet("exists/{email}")]
+        public int UserExists(string email)
+        {
+            var userId = userService.Exists(email);
+
+            if (userId == 0)
+            {
+                return 0;
+            }
+
+            return studentService.GetByUser(userId).Id;
+        }
+    }
+}
