99c3815336d17b6d4e6acbe6d25149dc5d9d94d8
Vítor Manfré <vitormd97@gmail.com>
2017-07-25 23:11:09 -0300

    Feat/add return user info login (#30)
    * Arruma erros de build
    
    * Adiciona retorno  com informações do usuario logado ao realizar o login

---
+++ TestIt.Utils/AuxEntities/LoggedUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestIt.Utils.AuxEntities
+{
+    public class LoggedUser
+    {
+        public int UserId { get; set; }
+        public int TeacherId { get; set; }
+        public int StudentId { get; set; }
+    }
+}
