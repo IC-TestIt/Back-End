ede5ba9abf849fec2473f8d9a6beac4c05bed29a
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-07-27 21:53:10 -0300

    Adiciona Controller Question

--- TestIt.API/ViewModels/Question/CreateQuestionViewModel.cs
+++ TestIt.API/ViewModels/Question/CreateQuestionViewModel.cs
@@ -1,23 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using TestIt.API.ViewModels.Validations.Test;
+using TestIt.API.ViewModels.Validations.Question;
 
-namespace TestIt.API.ViewModels.Test
+
+namespace TestIt.API.ViewModels.Question
 {
-    public class CreateTestViewModel : IValidatableObject
+    public class CreateQuestionViewModel : IValidatableObject
     {
-        public string Title { get; set; }
+        public string Answer { get; set; }
         public string Description { get; set; }
-        public int TeacherId { get; set; }
+        public double Value { get; set; }
+        public int TestId { get; set; }
 
         public IEnumerable<ValidationResult> Validate(System.ComponentModel.DataAnnotations.ValidationContext validationContext)
         {
-            var validator = new CreateTestViewModelValidator();
+            var validator = new CreateQuestionViewModelValidator();
             var result = validator.Validate(this);
             return result.Errors.Select(item => new ValidationResult(item.ErrorMessage, new[] { item.PropertyName }));
         }
-
     }
 }
