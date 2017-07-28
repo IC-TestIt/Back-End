e995626a0258e88eed5f39c8e56944b66cea0bdd
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-07-27 21:50:05 -0300

    Adiciona Question Service

---
+++ TestIt.Business/Services/QuestionService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using TestIt.Data.Abstract;
+using TestIt.Model.Entities;
+
+
+namespace TestIt.Business.Services
+{
+    public class QuestionService : IQuestionService
+    {
+        private IQuestionRepository questionRepository;
+
+        public QuestionService(IQuestionRepository questionRepository)
+        {
+            this.questionRepository = questionRepository;
+        }
+
+        public void Save(Question q)
+        {
+            questionRepository.Add(q);
+            questionRepository.Commit();
+        }
+
+    }
+}
