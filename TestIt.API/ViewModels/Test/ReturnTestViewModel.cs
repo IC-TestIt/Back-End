50787feea3847de805e30a1ede91324ebcdcc1f7
vitormdias <vitormd97@gmail.com>
2017-07-27 23:13:01 -0300

    Adiciona ReturnTestViewModel

---
+++ TestIt.API/ViewModels/Test/ReturnTestViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TestIt.API.ViewModels.Test
+{
+    public class ReturnTestViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int TeacherId { get; set; }
+        public ICollection<Model.Entities.Question> Questions { get; set; }
+    }
+}
