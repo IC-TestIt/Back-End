c1a234155661f16926e2a26a18c8fef5bb7e8693
vitormdias <vitormd97@gmail.com>
2017-07-29 21:33:35 -0300

    Adiciona repositorio questão dissertativa

---
+++ TestIt.Data/Repositories/EssayQuestionRepository.cs
@@ -0,0 +1,12 @@
+﻿using TestIt.Data.Abstract;
+using TestIt.Model.Entities;
+
+namespace TestIt.Data.Repositories
+{
+    public class EssayQuestionRepository : EntityBaseRepository<EssayQuestion>, IEssayQuestionRepository
+    {
+        public EssayQuestionRepository(TestItContext context)
+            : base(context)
+        { }
+    }
+}
