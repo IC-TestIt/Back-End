9874dbe69915b3c187555ca7b582b543311a31a3
vitormdias <vitormd97@gmail.com>
2017-07-29 21:32:56 -0300

    Adiciona modelo questão dissertativa

---
+++ TestIt.Model/Entities/EssayQuestion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestIt.Model.Entities
+{
+    public class EssayQuestion : IEntityBase
+    {
+        public EssayQuestion()
+        {
+            DateCreated = DateTime.Now;
+            DateUpdated = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        
+        public string Answer { get; set; }
+
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
