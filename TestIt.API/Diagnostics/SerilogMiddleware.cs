e659fcde6bddb3f39fe17b505d868d3a6c5aab83
vitormdias <vitor.dias@ventron.com.br>
2017-08-18 09:30:14 -0300

    Corrige momento de salvar o log

--- TestIt.API/Diagnostics/SerilogMiddleware.cs
+++ TestIt.API/Diagnostics/SerilogMiddleware.cs
@@ -44,22 +44,21 @@ namespace TestIt.API.Diagnostics
                 log.Write(level, MessageTemplate, httpContext.Request.Method, httpContext.Request.Path, statusCode, sw.Elapsed.TotalMilliseconds);
             }
             // Never caught, because `LogException()` returns false.
-            catch (Exception ex)
-            {
-                var log = BuildLog(ex);
-                logService.Save(log);
-            }
+            catch (Exception ex) when (LogException(httpContext, sw, ex)) { }
         }
 
-        //static bool LogException(HttpContext httpContext, Stopwatch sw, Exception ex)
-        //{
-        //    sw.Stop();
+        public bool LogException(HttpContext httpContext, Stopwatch sw, Exception ex)
+        {
+            sw.Stop();
 
-        //    LogForErrorContext(httpContext)
-        //        .Error(ex, MessageTemplate, httpContext.Request.Method, httpContext.Request.Path, 500, sw.Elapsed.TotalMilliseconds);
+            var log = BuildLog(ex);
+            logService.Save(log);
 
-        //    return false;
-        //}
+            LogForErrorContext(httpContext)
+                .Error(ex, MessageTemplate, httpContext.Request.Method, httpContext.Request.Path, 500, sw.Elapsed.TotalMilliseconds);
+
+            return false;
+        }
 
         static ILogger LogForErrorContext(HttpContext httpContext)
         {
@@ -83,10 +82,10 @@ namespace TestIt.API.Diagnostics
             var log = new Model.Entities.Log()
             {
                 Source = ex.Source,
-                Message = ex.Message,
+                Message = ex.InnerException.Message ?? null,
                 StackTrace = ex.StackTrace,
-                Method = trace.GetFrames().FirstOrDefault().GetMethod().Name,
-                Class = trace.GetFrames().FirstOrDefault().GetFileName()
+                Method = trace.GetFrames().FirstOrDefault().GetMethod().Name ?? null,
+                Class = trace.GetFrames().FirstOrDefault(x => x.GetFileName() != null).GetFileName() ?? null
             };
 
             return log;
