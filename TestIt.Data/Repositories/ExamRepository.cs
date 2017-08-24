83d1f2798a524d9a73ef85198243a2715417b6f4
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-08-24 20:05:59 -0300

    Adiciona Repository Avaliação

---
+++ TestIt.Data/Repositories/ExamRepository.cs
@@ -0,0 +1,12 @@
+﻿using TestIt.Data.Abstract;
+using TestIt.Model.Entities;
+
+namespace TestIt.Data.Repositories
+{
+    public class ExamRepository : EntityBaseRepository<Exam>, IExamRepository
+    {
+        public ExamRepository(TestItContext context)
+            : base(context)
+        { }
+    }
+}
