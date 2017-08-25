68078a3f4add90b0fb5eb34be894edfed6942b7b
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-08-24 21:16:14 -0300

    adiciona repository da questão respondida

---
+++ TestIt.Data/Repositories/AnsweredQuestionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestIt.Model.Entities;
+using TestIt.Data.Abstract;
+
+namespace TestIt.Data.Repositories
+{
+    public class AnsweredQuestionRepository : EntityBaseRepository<AnsweredQuestion>, IAnsweredQuestionRepository
+    {
+        public AnsweredQuestionRepository(TestItContext context)
+            : base(context)
+        { }
+    }
+}
