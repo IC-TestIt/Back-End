3a1498faaf3eb4eca56d7979873c92b21a6e83cd
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-08-28 20:14:51 -0300

    Create ExamController

---
+++ TestIt.API/Controllers/ExamController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestIt.API.ViewModels.Exam;
+using TestIt.Business;
+using TestIt.Model.Entities;
+
+namespace TestIt.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ExamController : Controller
+    {
+        private IExamService examService;
+
+        public ExamController(IExamService examService)
+        {
+            this.examService = examService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]CreateExamViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Exam exam = Mapper.Map<Exam>(viewModel);
+
+            examService.Save(exam);
+
+            OkObjectResult result = Ok(new { examId = exam.Id });
+
+            return result;
+        }
+    }
+}
