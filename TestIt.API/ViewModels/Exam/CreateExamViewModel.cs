c423dd5cb8174f8b8876a13f8509869fc50cb048
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-08-28 20:14:37 -0300

    Create ExamViewModels

---
+++ TestIt.API/ViewModels/Exam/CreateExamViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TestIt.API.ViewModels.Validations.Exam;
+
+namespace TestIt.API.ViewModels.Exam
+{
+    public class CreateExamViewModel : IValidatableObject
+    {
+        public int StudentId { get; set; }
+        public int ClassTestsId { get; set; }
+
+
+        public IEnumerable<ValidationResult> Validate(System.ComponentModel.DataAnnotations.ValidationContext validationContext)
+        {
+            var validator = new CreateExamViewModelValidator();
+            var result = validator.Validate(this);
+            return result.Errors.Select(item => new ValidationResult(item.ErrorMessage, new[] { item.PropertyName }));
+        }
+    }
+}
---
+++ TestIt.API/ViewModels/Validations/Exam/CreateExamViewModelValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestIt.API.ViewModels.Exam;
+
+namespace TestIt.API.ViewModels.Validations.Exam
+{
+    public class CreateExamViewModelValidator : AbstractValidator<CreateExamViewModel>
+    {
+        public CreateExamViewModelValidator()
+        {
+            RuleFor(exam => exam.StudentId).NotEmpty().WithMessage("StudentId cannot be empty");
+            RuleFor(exam => exam.ClassTestsId).NotEmpty().WithMessage("ClassTestsId cannot be empty");
+        }
+    }
+}
