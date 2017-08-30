a15d15545d83f49f35050b585180bf1790955f78
Medson <medson.oliveira.jr@gmail.com>
2017-08-30 01:56:07 -0300

    adiciona return asnweredQuestionViewModel

---
+++ TestIt.API/ViewModels/AnsweredQuestion/ReturnAnsweredQuestionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Org.BouncyCastle.Asn1.Misc;
+using TestIt.Model.Entities;
+
+namespace TestIt.API.ViewModels.AnsweredQuestion
+{
+    public class ReturnAnsweredQuestionViewModel
+    {
+        public int Id { get; set; }
+        public virtual string EssayAnswer { get; set; }
+        public virtual Alternative Alternative { get; set; }
+        public Model.Entities.Question Question { get; set; }
+        public int ExamId { get; set; }
+    }
+}
