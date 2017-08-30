440f6f5b4dc5ce6f7e0a13dcd0d1b17564f25d0e
Medson <medson.oliveira.jr@gmail.com>
2017-08-30 01:56:33 -0300

    Adiciona return exam view model

---
+++ TestIt.API/ViewModels/Exam/ReturnExamViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TestIt.Model.Entities;
+
+namespace TestIt.API.ViewModels.Exam
+{
+    public class ReturnExamViewModel
+    {
+        public int StudentId { get; set; }
+        public int ClassTestsId { get; set; }
+        public ICollection<AnsweredQuestion.ReturnAnsweredQuestionViewModel> AnsweredQuestions { get; set; }
+    }
+}
