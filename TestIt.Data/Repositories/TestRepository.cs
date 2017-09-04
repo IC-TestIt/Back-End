9ff24c1ab1f8c1ebce2b4f5d22f70b7c144292ef
vitormdias <vitormd97@gmail.com>
2017-09-03 23:23:33 -0300

    Muda repositorios

--- TestIt.Data/Repositories/TestRepository.cs
+++ TestIt.Data/Repositories/TestRepository.cs
@@ -10,17 +10,15 @@ namespace TestIt.Data.Repositories
 {
     public class TestRepository : EntityBaseRepository<Test>, ITestRepository
     {
-        private TestItContext context;
         public TestRepository(TestItContext context)
             : base(context)
         {
-            this.context = context;
         }
 
         public Test GetFull(int id)
         {
-            var question = (from a in context.Questions
-                        join b in context.Tests on a.TestId equals b.Id
+            var question = (from a in _context.Questions
+                        join b in _context.Tests on a.TestId equals b.Id
                         where a.TestId == id
                         select a).Include(x => x.EssayQuestion).Include(x => x.Test).Include(x => x.AlternativeQuestion.Alternatives).ToList();
 
--- TestIt.Data/Repositories/UserRepository.cs
+++ TestIt.Data/Repositories/UserRepository.cs
@@ -9,18 +9,16 @@ namespace TestIt.Data.Repositories
 {
     public class UserRepository : EntityBaseRepository<User>, IUserRepository
     {
-        private TestItContext context;
         public UserRepository(TestItContext context)
             : base(context)
         {
-            this.context = context;
         }
 
         public List<User> ClassUsers (int classId)
         {
-            var users = (from a in context.ClassStudents
-                         join b in context.Students on a.StudentId equals b.Id
-                         join c in context.Users on b.UserId equals c.Id
+            var users = (from a in _context.ClassStudents
+                         join b in _context.Students on a.StudentId equals b.Id
+                         join c in _context.Users on b.UserId equals c.Id
                          where a.ClassId == classId
                          select c).ToList();
 
