f49b41e0b727d942badfb3e945bf13d61c7f8b64
vitormdias <vitor.dias@ventron.com.br>
2017-09-09 20:49:28 -0300

    Adiciona camada para metodos extensivos

---
+++ TestIt.Utils/Extend/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TestIt.Utils.Extend
+{
+    public static class Extensions
+    {
+        public static int Count(this string value, IEnumerable<string> subStrings)
+        {
+            var result = subStrings.Select(x =>
+                               new
+                               {
+                                   word = x,
+                                   exists = Regex.IsMatch(value, @"\b" + x + @"\b")
+                               }
+                            );
+
+            return result.Count(x => x.exists);
+        }
+    }
+}
