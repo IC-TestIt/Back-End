f149061458f2dab4e299dd29898ccd929d38a008
Medson de Oliveira Junior <medson.oliveira.jr@gmail.com>
2017-09-13 17:18:11 -0300

    Inclui quantidade de alunos da turma (#75)

--- TestIt.API/ViewModels/Class/TeacherClassesViewModel.cs
+++ TestIt.API/ViewModels/Class/TeacherClassesViewModel.cs
@@ -8,6 +8,7 @@ namespace TestIt.API.ViewModels.Class
     public class TeacherClassesViewModel
     {
         public string Description { get; set; }
+        public int Size { get; set; }
         public int Id { get; set; }
     }
 }
