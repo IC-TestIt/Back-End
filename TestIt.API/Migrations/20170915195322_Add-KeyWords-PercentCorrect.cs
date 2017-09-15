87fe47978c590f44133bfb2643380f4e2d34e178
vitormdias <vitor.dias@ventron.com.br>
2017-09-15 17:01:27 -0300

    Adiciona migrations correção

---
+++ TestIt.API/Migrations/20170915195322_Add-KeyWords-PercentCorrect.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestIt.API.Migrations
+{
+    public partial class AddKeyWordsPercentCorrect : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "KeyWords",
+                table: "EssayQuestions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "PercentCorrect",
+                table: "AnsweredQuestions",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "KeyWords",
+                table: "EssayQuestions");
+
+            migrationBuilder.DropColumn(
+                name: "PercentCorrect",
+                table: "AnsweredQuestions");
+        }
+    }
+}
