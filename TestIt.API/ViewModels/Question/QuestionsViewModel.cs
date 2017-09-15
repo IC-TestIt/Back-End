20e47a43c35836521bd4d786d02b9d030860b9f0
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-09-15 00:00:29 -0300

    Cria QuestionsViewModel

---
+++ TestIt.API/ViewModels/Question/QuestionsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestIt.API.ViewModels.Question
+{
+    public class QuestionsViewModel  
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public int TestId { get; set; }
+        public string Answer { get; set; }
+        public virtual List<AlternativeViewModel> Alternatives { get; set; }
+    }
+}
