e17e4b499767e1063d115db16b0102882f576146
vitormdias <vitormd97@gmail.com>
2017-09-17 22:21:42 -0300

    Adiciona ViewModel para ClassTests

---
+++ TestIt.API/ViewModels/Class/ClassTestsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestIt.API.ViewModels.Class
+{
+    public class ClassTestsViewModel
+    {
+        public int Id { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string ClassName { get; set; }
+        public string TestTitle { get; set; }
+    }
+}
