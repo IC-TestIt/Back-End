0cc19e1e5a56c094eae68692211e0b8294f82499
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-10-05 21:02:09 -0300

    Adiciona Método GetTeacherTest no Repository

--- TestIt.Data/Repositories/TestRepository.cs
+++ TestIt.Data/Repositories/TestRepository.cs
@@ -2,6 +2,10 @@
 using Microsoft.EntityFrameworkCore;
 using TestIt.Data.Abstract;
 using TestIt.Model.Entities;
+using TestIt.Model.DTO;
+using System.Collections.Generic;
+using TestIt.Model;
+using System;
 
 namespace TestIt.Data.Repositories
 {
@@ -20,6 +24,65 @@ namespace TestIt.Data.Repositories
                         select a).Include(x => x.EssayQuestion).Include(x => x.Test).Include(x => x.AlternativeQuestion.Alternatives).OrderBy(x => x.Order).ToList();
 
             return question.FirstOrDefault().Test ?? null;
+        }
+
+        public IEnumerable<TeacherTestsDTO> GetTeacherTests(int id)
+        {
+            var tests = (from a in Context.Tests
+                         join b in Context.ClassTests on a.Id equals b.TestId
+                         join c in Context.Exams on b.Id equals c.ClassTestsId
+                         where a.TeacherId == id && c.TotalGrade == 0
+                         select new TeacherTestsDTO
+                         {
+                             Title = a.Title,
+                             Description = a.Description,
+                             Id = a.Id,
+                             Status = EnumTestStatus.Uncorrected
+                         }).ToList();
+
+            tests.AddRange(from a in Context.Tests
+                           join b in Context.ClassTests on a.Id equals b.TestId
+                           join c in Context.Exams on b.Id equals c.ClassTestsId
+                           where a.TeacherId == id && c.TotalGrade > 0 
+                           select new TeacherTestsDTO
+                           {
+                               Title = a.Title,
+                               Description = a.Description,
+                               Id = a.Id,
+                               Status = EnumTestStatus.Corrected
+                           });
+
+            tests.AddRange(from a in Context.Tests
+                           join b in Context.ClassTests on a.Id equals b.TestId
+                           where a.TeacherId == id && b.EndDate > DateTime.Now 
+                           select new TeacherTestsDTO
+                           {
+                               Title = a.Title,
+                               Description = a.Description,
+                               Id = a.Id,
+                               Status = EnumTestStatus.Applied
+                           });
+
+            tests.AddRange(from a in Context.Tests
+                           where a.TeacherId == id && !Context.ClassTests.Any(b=>(b.TestId == a.Id))
+                           select new TeacherTestsDTO
+                           {
+                               Title = a.Title,
+                               Description = a.Description,
+                               Id = a.Id,
+                               Status = EnumTestStatus.notApplied
+                           });
+
+            foreach (TeacherTestsDTO tt in tests)
+            {
+                tt.ClassTestsApplied = (from a in Context.Classes
+                                        join b in Context.ClassTests on a.Id equals b.ClassId
+                                        join c in Context.Tests on b.TestId equals c.Id
+                                        where c.TeacherId == id
+                                        select a).ToList();
+            }
+
+            return tests;
         }
     }       
 }
