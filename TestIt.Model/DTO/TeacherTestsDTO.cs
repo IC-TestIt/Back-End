2c1ed6107c221fdda633b65259848aa064eba930
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-10-05 21:00:27 -0300

    Cria TeacherTestsDTO

---
+++ TestIt.Model/DTO/TeacherTestsDTO.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using TestIt.Model.Entities;
+
+namespace TestIt.Model.DTO
+{
+    public class TeacherTestsDTO
+    {
+        public TeacherTestsDTO()
+        {
+            ClassTestsApplied = new List<Class>();
+        }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public EnumTestStatus Status { get; set; }
+        public IEnumerable<Class> ClassTestsApplied { get; set; }
+    }
+}
