0dee0b8111412313ee1179e08d725f2e706d27fa
Luiz Soares <luizguilherme.soares97@gmail.com>
2017-11-01 19:46:20 -0200

    Adiciona Teacherclasses e Teacherclass DTOs

---
+++ TestIt.Model/DTO/TeacherClassesDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+
+namespace TestIt.Model.DTO
+{
+    public class TeacherClassesDTO
+    {
+        public TeacherClassesDTO()
+        {
+            Classes = new List<TeacherClassDTO>();
+        }
+
+        public int TotalClasses { get; set; }
+
+        public TeacherClassDTO BestClass { get; set; }
+        public TeacherClassDTO WorseClass { get; set; }
+
+        public IEnumerable<TeacherClassDTO> Classes { get; set; }
+
+    }
+}
