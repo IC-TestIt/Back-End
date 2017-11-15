83a102fb8664b954da9042028bb7be0c12b1bb06
vitormdias <vitor.dias@ventron.com.br>
2017-11-15 21:12:46 -0200

    adiciona migrações

--- TestIt.API/Migrations/20171115230533_Add-Is-Published-Class-Test.Designer.cs
+++ TestIt.API/Migrations/20171115230533_Add-Is-Published-Class-Test.Designer.cs
@@ -8,9 +8,10 @@ using TestIt.Data;
 namespace TestIt.API.Migrations
 {
     [DbContext(typeof(TestItContext))]
-    partial class TestItContextModelSnapshot : ModelSnapshot
+    [Migration("20171115230533_Add-Is-Published-Class-Test")]
+    partial class AddIsPublishedClassTest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")
