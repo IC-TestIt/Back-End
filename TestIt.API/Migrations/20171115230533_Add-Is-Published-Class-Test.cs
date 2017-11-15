e4dd96559b99a28484af1ce64a3d2b90c44acd43
Vítor Manfré <vitormd97@gmail.com>
2017-11-15 21:24:24 -0200

    Feat/class test is published (#108)
    * adiciona campo is published
    
    * adiciona migrações

---
+++ TestIt.API/Migrations/20171115230533_Add-Is-Published-Class-Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestIt.API.Migrations
+{
+    public partial class AddIsPublishedClassTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "ClassTests",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "ClassTests");
+        }
+    }
+}
