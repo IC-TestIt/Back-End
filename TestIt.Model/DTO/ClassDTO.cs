085793dc1cf29823bdcfec38b834a86b8a8e4f20
vitormdias <vitor.dias@ventron.com.br>
2017-11-18 17:41:59 -0200

    Cria DTO para detalhes da turma

---
+++ TestIt.Model/DTO/ClassDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace TestIt.Model.DTO
+{
+    public class ClassDTO
+    {
+        public int TeacherId { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<ClassTestStudentDTO> Students { get; set; }
+        public IEnumerable<TeacherTestsDTO> Tests { get; set; }
+    }
+}
