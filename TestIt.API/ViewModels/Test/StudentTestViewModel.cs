24d9103f4ee8e5d24844a325283c8aa960f38baf
vitormdias <vitor.dias@ventron.com.br>
2017-11-18 23:38:48 -0200

    Adiciona data de inicio na view model das provas do aluno

--- TestIt.API/ViewModels/Test/StudentTestViewModel.cs
+++ TestIt.API/ViewModels/Test/StudentTestViewModel.cs
@@ -7,6 +7,7 @@ namespace TestIt.API.ViewModels.Test
         public string Name { get; set; }
         public string ClassName { get; set; }
         public string TeacherName { get; set; }
+        public DateTime BeginDate { get; set; }
         public DateTime EndDate { get; set; }
         public int ClassTestId { get; set; }
     }
